e5aea745f3a2eab045369d8b1d8b347a97d435ac
sonaliluthar <sonaliluthar@gmail.com>
2018-10-13 09:05:44 -0400

    open json link in new tab on refresh

---
+++ src/Asset.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class Asset extends Component {
+  componentWillMount() {
+    setTimeout(() => {
+      const response = {
+        file: "https://www.twinthread.com/code-challenge/assets.txt"
+      };
+      window.open(response.file);
+    }, 100);
+  }
+
+  render() {
+    return <div />;
+  }
+}
